73bd9cc26cf45511ff189f4dc20d87dcfcfe6936
a-llave <43776370+a-llave@users.noreply.github.com>
2020-03-04 15:42:34 +0100

    Fix the bug in Wiener and Fontaine. Now the final product is computed correctly

--- src/Fontaine.py
+++ src/Fontaine.py
@@ -2,22 +2,23 @@
 """
 Description: Class Fontaine for ENS mini project
 
-Author: Victor Perrin - ENS Rennes
-Date: 30/01/2020
+Author: Adrien Llave - CentraleSupelec
+Date: 04/03/2020
 
-Version: 1.0
+Version: 2.1
 
 Date    | Auth. | Vers.  |  Comments
 30/01/20  VPe     1.0       Initialization
 06/02/20  VPe     2.0       added class Fontaine débruitage un peu
-
-
+04/03/20  ALl     2.1       - Bug fix: wrong way to implement the product between filter and input signal
+                            - Ensure not doint log(0) in scale factor estimation
 
 """
 
 import numpy as np
 import sys
 import scipy.stats as scistat
+
 
 class Fontaine:
         def __init__(self, nb_fft, RATE, win_size, delta_n=0.16, delta_s=0.09, alpha_n=1.8, alpha_s=1.2):
@@ -38,14 +39,15 @@ class Fontaine:
 
             self.s_m = np.zeros((nb_frq, self.N_s), complex)
             self.n_m = np.zeros((nb_frq, self.N_n), complex)
+            self.eps_f = 10 ** -8
 
         def process(self, x_v, s_v, n_v):
             # estimation
             self.s_m = np.concatenate((self.s_m[:, 1:], s_v), axis=1)
             self.n_m = np.concatenate((self.n_m[:, 1:], n_v), axis=1)
 
-            lambda_s_v = np.exp((1 / self.alpha_s) * np.mean(np.log(np.abs(self.s_m)), axis=1) + self.gamma * (1 - 1 / self.alpha_s))
-            lambda_n_v = np.exp((1 / self.alpha_n) * np.mean(np.log(np.abs(self.n_m)), axis=1) + self.gamma * (1 - 1 / self.alpha_n))
+            lambda_s_v = np.exp((1 / self.alpha_s) * np.mean(np.log(np.abs(self.s_m)+self.eps_f), axis=1) + self.gamma * (1 - 1 / self.alpha_s))
+            lambda_n_v = np.exp((1 / self.alpha_n) * np.mean(np.log(np.abs(self.n_m)+self.eps_f), axis=1) + self.gamma * (1 - 1 / self.alpha_n))
 
             phi_s_f = scistat.levy.median(loc=0, scale=2 * np.cos(np.pi * self.alpha_s / 4)**(2/self.alpha_s))
             phi_n_f = scistat.levy.median(loc=0, scale=2 * np.cos(np.pi * self.alpha_n / 4)**(2/self.alpha_n))
@@ -63,6 +65,6 @@ class Fontaine:
             # plt.plot(w_v)
             # plt.draw()
             # plt.pause(0.001)
-
-            return w_v * x_v
+            s_estim_v = w_v[:, None] * x_v
+            return s_estim_v
 
--- src/Wiener.py
+++ src/Wiener.py
@@ -1,14 +1,15 @@
 """
 Description:    wiener filter
 
-Author: Martin Witt - ENS Rennes
-Date: 30/01/2020
+Author: Adrien Llave - CentraleSupelec
+Date: 04/03/2020
 
-Version: 1
+Version: 2.1
 
 Date    | Auth. | Vers.  |  Comments
 30/01/20   MWi     1.0       Initialization
 06/02/20   MWi     2.0       Complete attenue
+04/03/20  ALl       2.1      Bug fix: wrong way to implement the product between filter and input signal
 
 """
 
@@ -16,6 +17,7 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 import utils_llave as u
+
 
 class Wiener:
     def __init__(self, nb_fft, RATE, win_size, delta_n=0.16, delta_s=0.09):
@@ -31,7 +33,8 @@ class Wiener:
 
         self.s_m = np.zeros((nb_frq, self.N_s), complex)
         self.n_m = np.zeros((nb_frq, self.N_n), complex)
-        
+        self.fig = None
+
     def process(self, x_v, s_v, n_v):
         # estimation
         self.s_m = np.concatenate((self.s_m[:, 1:], s_v), axis=1)
@@ -42,18 +45,31 @@ class Wiener:
 
         # filter
         w_v = sigma_s2_v / (sigma_n2_v + sigma_s2_v)
-        w_v = sigma_s2_v / (sigma_n2_v + sigma_s2_v)
+        # nb_freq = sigma_s2_v.shape[0]
+        # w_v = np.zeros(nb_freq)
+        # for id_freq in range(nb_freq):
+        #     w_v[id_freq] = sigma_s2_v[id_freq] / (sigma_n2_v[id_freq] + sigma_s2_v[id_freq])
+        # self.w_v = self.coef * w_v + (1-self.coef) * self.w_v
+        # PLOT
+        # self.fig_update(sigma_s2_v, sigma_n2_v, w_v)
+        s_estim_v = w_v[:, None] * x_v
+        return s_estim_v
 
-        # w_v[w_v < u.db2mag(-40)] = u.db2mag(-40)
-        # plt.clf()
-        # plt.plot(sigma_s2_v)
-        # plt.plot(sigma_n2_v)
-        # plt.draw()
-        # plt.pause(0.001)
-        # plt.clf()
-        # plt.plot(w_v)
-        # plt.draw()
-        # plt.pause(0.001)
+    def fig_init(self):
+        self.fig = plt.figure()
+        self.axs = [self.fig.add_subplot(1, 2, 1 + ii) for ii in range(2)]
+        return
 
-        return w_v * x_v
-
+    def fig_update(self, sigma_s2_v, sigma_n2_v, w_v):
+        if self.fig is None:
+            self.fig_init()
+        for ax in self.axs:
+            ax.cla()
+        w_v[w_v < u.db2mag(-40)] = u.db2mag(-40)
+        self.axs[0].plot(sigma_s2_v, label='Source')
+        self.axs[0].plot(sigma_n2_v, label='Noise')
+        self.axs[0].legend()
+        self.axs[1].plot(w_v)
+        plt.draw()
+        plt.pause(0.01)
+        return
